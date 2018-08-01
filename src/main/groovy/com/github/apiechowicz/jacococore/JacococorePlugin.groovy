6d25034a2b030e4c3eef2dce7af529e91499d87d
Andrzej Piechowicz <15264663+apiechowicz@users.noreply.github.com>
2018-08-01 12:17:57 +0200

    Create plugin skeleton

---
+++ src/main/groovy/com/github/apiechowicz/jacococore/JacococorePlugin.groovy
@@ -0,0 +1,11 @@
+package com.github.apiechowicz.jacococore
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class JacococorePlugin implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+
+    }
+}
